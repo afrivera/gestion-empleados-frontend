40983a46ae20bd85eca5f6d88414a1bc5e7885db
afrivera <andresrfelip@hotmail.com>
2022-09-29 22:13:15 -0500

    config title de la pagina y maquetación del navbar

--- src/app/lista-empleados/lista-empleados.component.ts
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -10,29 +11,17 @@ export class ListaEmpleadosComponent implements OnInit {
 
   empleados!:Empleado[];
 
-  constructor() { }
+  constructor(
+    private empleadoService: EmpleadoService
+  ) { }
 
   ngOnInit(): void {
-    this.empleados = [
-      {
-        id: 1,
-        nombre: "Andres",
-        apellido: "Rivera",
-        email:"andresrfelip@hotmail.com"
-      },
-      {
-        id: 2,
-        nombre: "Juan David",
-        apellido: "Rivera",
-        email:"juandavid@hotmail.com"
-      },
-      {
-        id: 3,
-        nombre: "Cristian",
-        apellido: "Rivera",
-        email:"cristianrivera@hotmail.com"
-      }
-    ]
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.empleadoService.getAllEmpleado()
+        .subscribe(e=> this.empleados = e);
   }
 
 }
